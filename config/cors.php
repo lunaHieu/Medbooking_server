f730eb69ca92c119fb53448436974cc40bc21e1b
lunaHieu <nguyenvanhieu040205@gmail.com>
2026-05-06 13:12:11 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,8 @@ return [
 
     'allowed_origins' => [
     'http://localhost:3000',
-     'http://127.0.0.1:3000', 
+     'http://127.0.0.1:3000',
+     "*" 
 ],
 
     'allowed_origins_patterns' => [],
